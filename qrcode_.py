f127cac3a86e9e5dbcaac4bc3e45dd7cc8066f01
duck <shangaoxiangnb@gmail.com>
2023-10-21 22:38:09 +0800

    存

--- qrcode_.py
+++ qrcode_.py
@@ -1,5 +1,5 @@
 import qrcode
-img = qrcode.make("http://192.168.1.24:8000/teacher/index/")
+img = qrcode.make("http://192.168.1.24:8080/#/")
 with open('a.png', 'wb') as f:
     img.save(f)
 
--- teacher/views.py
+++ teacher/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .my_auch import *
+from .models import *
 
 class index(APIView):
     authentication_classes = []
@@ -10,3 +11,13 @@ class index(APIView):
 class my_class(APIView):
     def get(self,request):
         return Response({ "code":"200", "moey":"OK", "teacher":"22 shigong 1", })
+    def post(self,request):
+        user = request.get("user")
+        pawd = request.get("pawd")
+        users = UserInfo()
+        users.user = user
+        users.pswd = pawd
+        users.save()
+
+class a(APIView):
+    pass
